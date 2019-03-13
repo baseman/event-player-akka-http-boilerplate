c47aa5d5d1dbf2655330ba4e4253cb7e1c412588
baseman <steve.hammond@outlook.com>
2019-03-13 19:55:38 -0400

    add load scenarios

---
+++ my-scenarios/src/load/scala/my/artifact/myeventplayer/scenarios/load/MyLoad.scala
@@ -0,0 +1,28 @@
+package my.artifact.myeventplayer.scenarios.load
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+class MyLoad extends Simulation {
+
+	val httpProtocol = http
+		.baseURL("http://localhost:8080")
+		.contentTypeHeader("application/vnd.my.artifact.myeventplayer.common.command.MyCreateCommand.api.v1+json")
+
+	val headers_0 = Map("accept" -> "application/json")
+
+    val uri1 = "http://localhost:8080"
+
+	val scn = scenario("MyLoad").exec(
+			http("request_0")
+			.post(uri1 + "/my/cmd")
+			.headers(headers_0)
+			.body(
+				StringBody(
+					"{\"myInitialVal\":\"initial blah\"}"
+				)
+			)
+		)
+
+	setUp(scn.inject(atOnceUsers(1000))).protocols(httpProtocol)
+}
