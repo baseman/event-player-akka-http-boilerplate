91703ab6d5fb3e7da6c4642d090edced4e5e60dc
Stephen Hammond <hammond.stephen@gmail.com>
2021-06-01 16:43:22 -0400

    update gatling + kotlin version

--- buildSrc/src/main/kotlin/co/remotectrl/ctrl/shell/cli/Versions.kt
+++ buildSrc/src/main/kotlin/co/remotectrl/ctrl/shell/cli/Versions.kt
@@ -7,10 +7,6 @@ object Versions {
 
     object Dependencies {
         const val kotlin = "1.5.10"
-        const val serialization = "0.14.0"
-        const val coroutine = "1.3.3"
-        const val springBootVersion = "2.0.3.RELEASE"
         const val akkaVersion = "10.1.4"
-        const val sonarVersion = "2.7.1"
     }
 }
